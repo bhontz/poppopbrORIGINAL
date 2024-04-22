89e1b7b44936f5bf7546af3cb8ae9e55cce1a46b
brad <bhontz@paiwhq.com>
2024-04-22 16:17:32 -0700

    learning the bootstrap grid system

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,23 @@ function App() {
   };
 
   return (
-    <div>
-      <Alert>Hey you got a warning going on here!</Alert>
-      <ListGroup
-        items={items}
-        title="Da Cities Here"
-        onSelectItem={handleSelectItem}
-      />
+    <div className="container-xl my-5">
+      <div className="row">
+        <div className="col">
+          <Alert>Hey you got a warning going on here!</Alert>
+          <ListGroup
+            items={items}
+            title="Da Cities Here"
+            onSelectItem={handleSelectItem}
+          />
+        </div>
+        <div className="col">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
+          consequuntur porro ea in debitis similique nihil blanditiis culpa! Sit
+          a sed voluptas veniam nam, eveniet officia delectus. Molestiae, velit
+          sunt!
+        </div>
+      </div>
     </div>
   );
 }
