29d1dbc21a53b8baac6d90e2ffb569ee997aff47
brad <bhontz@paiwhq.com>
2024-04-23 13:53:30 -0700

    attempt at bootstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,43 @@ function App() {
 
   return (
     <div className="container-xl my-5">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar w/ text
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>{" "}
       <div className="row">
         <div className="col">
           <Alert>Hey you got a warning going on here!</Alert>
